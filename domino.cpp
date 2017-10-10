c1b3b9b6ae6bd8341c775c7f52c91bbef8b34eb2
Mike Su <kyatapi@gmail.com>
2017-10-10 11:17:01 +0800

    Add test case for no toppling

--- domino.cpp
+++ domino.cpp
@@ -5,6 +5,32 @@ extern void run_test(int argc, char **argv);
 inline void run_test(int argc, char **argv) {}
 #endif // !ONLINE_JUDGE
 
+#include <sstream>
+
+using namespace std;
+
+class DominoShow {
+public:
+    DominoShow() {}
+    ~DominoShow() {}
+
+    void proceed() {}
+    size_t number_of_fall() { return 0; }
+
+    friend istream & operator >> (istream &is, DominoShow &rhs);
+};
+
+istream & operator >> (istream &is, DominoShow &rhs) {
+    return is;
+}
+
+size_t proceed_domino_show(istream& iss) {
+    DominoShow show;
+    iss >> show;
+    show.proceed();
+    return show.number_of_fall();
+}
+
 int main(int argc, char** argv) {
     run_test(argc, argv);
 
--- test_cases.cpp
+++ test_cases.cpp
@@ -1,2 +1,11 @@
 #include "test_cases.h"
+#include <sstream>
 
+using namespace std;
+
+extern size_t proceed_domino_show(istream& iss);
+
+TEST(DominoShow, HandleNoToppling) {
+    istringstream iss("3 0 0");
+    ASSERT_EQ(proceed_domino_show(iss), 0);
+}
