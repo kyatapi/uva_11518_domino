775d0cd38673f8170ff70657c1f51fa447596c8c
Mike Su <kyatapi@gmail.com>
2017-10-10 11:24:08 +0800

    Add main logic: read number of test cases and proceed them

--- domino.cpp
+++ domino.cpp
@@ -34,5 +34,10 @@ size_t proceed_domino_show(istream& iss) {
 int main(int argc, char** argv) {
     run_test(argc, argv);
 
+    size_t number_of_show = 0;
+    for (cin >> number_of_show; number_of_show >= 0; --number_of_show) {
+        cout << proceed_domino_show(cin) << endl;
+    }
+
     return 0;
 }
